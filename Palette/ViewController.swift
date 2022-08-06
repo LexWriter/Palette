fdbba0e765894b647a100740c4f5b2f2f9b2a460
LexWritter <rolleks@mail.ru>
2022-08-06 20:38:21 +0300

    minor major

--- Palette/ViewController.swift
+++ Palette/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var paletteView: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func slidersChanged() {
+        
+        redLabel.text = String(round(100 * redSlider.value)/100)
+        greenLabel.text = String(round(100 * greenSlider.value)/100)
+        blueLabel.text = String(round(100 * blueSlider.value)/100)
+        
+        paletteView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
+        }
 }
 
